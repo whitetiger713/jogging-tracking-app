8c4eaf5a24d7d611eac0945a7e3b4aec66b44022
whitetiger713 <jameshart713@gmail.com>
2018-09-08 01:00:10 +0800

    add verify UI

---
+++ frontend/src/pages/Auth/Verify.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Style.css';
+
+class VerifyPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.userForm = this.userForm.bind(this);
+    this.logSuccess = this.logSuccess.bind(this);
+    sessionStorage.clear();
+  };
+  logSuccess(){
+    this.props.history.push("/app");
+    window.location.reload();
+  }
+  userForm = (e) => {
+    const that = this;
+    e.preventDefault();
+    var email = sessionStorage.getItem('verifyemail');
+    console.log(email)
+    var key = this.refs.key.value;
+    axios.get(`http://localhost:8080/user/verify?id=${key}`,{
+      email: email
+    })
+    .then(function (response) {
+      if(response.data.state === 1){
+        toast.success(response.data.message);
+        that.logSuccess();
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="login mt-5">
+          <div className="model-content model-info">
+            <div className="model-header">
+              <h2>Email Verify</h2>
+              <div className="lo-im">
+                <img src="/images/avatar_2x.png" className="user-image" title=" user " alt="user" />
+              </div>
+            </div>
+            <div className="model-body model-spa">
+              <div className="login-form">
+                <form onSubmit={ this.userForm }>
+                  <input type="text" ref="key"  placeholder="TokenKey" required autoFocus />
+                  <input type="submit" value="Send Email"/>
+                </form>
+                <p><a href="/login">Login</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default VerifyPage;
+
